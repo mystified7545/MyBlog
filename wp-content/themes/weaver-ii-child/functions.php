8b2b8764bb010df45bceff516a75fc6f01d17d30
mystified7545 <pratik.webrix@gmail.com>
2016-01-16 03:52:11 -0800

    created method to return the page post

--- wp-content/themes/weaver-ii-child/functions.php
+++ wp-content/themes/weaver-ii-child/functions.php
@@ -9,20 +9,7 @@
 add_action('admin_init','hide_editor');
 
 function hide_editor() {
-
-    // Get the Post ID.
-    $post_id = $_GET['post'] ? $_GET['post'] : $_POST['post_ID'] ;
-   // die($post_id);
-    //echo("This is the value".$post_id);
-
-    if( !isset( $post_id ) ) return;
-
-   $post =  get_post($post_id);
-
-    // Get the name of the Page Template file.
-    //$template_file = get_post_meta($post_id, '_wp_page_template', true);
-
-    //die($template_file);
+    $post=get_pagepost();
 
     if(strtolower($post->post_title) == 'about'){ // edit the template name
         remove_post_type_support('page', 'editor');
@@ -58,5 +45,20 @@ function my_toolbars( $toolbars )
     unset( $toolbars['Full' ]);
     // return $toolbars - IMPORTANT!
     return $toolbars;
+}
+
+function get_pagepost()
+{
+    // Get the Post ID.
+    $post_id = $_GET['post'] ? $_GET['post'] : $_POST['post_ID'] ;
+    echo("from inside".absint($post_id));
+
+    if( !isset( $post_id ) ) return;
+    $post =  get_post($post_id);
+
+    // Get the name of the Page Template file.
+    //$template_file = get_post_meta($post_id, '_wp_page_template', true);
+    //die($template_file);
+    return $post;
 }
 ?>
